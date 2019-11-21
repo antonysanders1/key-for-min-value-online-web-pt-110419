3dde87e62386fae85afca5e0d09fd9eec3079b5e
Antony Sanders <antony.devon92@gmail.com>
2019-11-21 16:18:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,16 @@
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
  
 def key_for_min_value(name_hash)
+  key_for_min = nil 
+  lowest_value = nil 
   
+  
+  name_hash.each do ||
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      key_for_min = key
+    end
+    
+  end 
+  key_for_min
 end 
